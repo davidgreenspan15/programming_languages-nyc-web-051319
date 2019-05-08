ca74386b178e8eaee85bbd60cebcb97a137e074b
David Greenspan <ramigreenspan@gmail.com>
2019-05-08 07:33:29 -0400

    programming languages

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,3 +1,17 @@
-def reformat_languages(languages)
-  # your code here
+require 'pry'
+
+ def reformat_languages(languages)
+new_hash = {}
+
+   languages.each do |infinity, lang|
+    lang.each do |speclang, type|
+      if new_hash.has_key?(speclang)
+        new_hash[speclang][:style] << infinity
+      else
+        new_hash[speclang] = type
+        new_hash[speclang][:style] = [infinity]
+      end
+    end
+  end
+   new_hash
 end
